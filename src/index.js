7e86e4c60aa80ee09bcf2bf9d7fe5bd68ff974fd
Surta <pasha.surta@mail.ru>
2021-02-15 11:09:17 +0300

    redux. Создается при помощи createStore(f()). Для подписки на каждое обновление store используется subscribe(). Для изменения state используется dispatch({ type: 'Some type'  })

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { createStore } from 'redux'
 
 const reducer = (state = 0, action) => {
     switch (action.type){
@@ -8,8 +9,8 @@ const reducer = (state = 0, action) => {
     }
 }
 
-let state = reducer(undefined, {})
-state = reducer(state, { type: 'INC' })
-console.log(state)
-state = reducer(state, { type: 'INC' })
-console.log(state)
+const store = createStore(reducer)
+store.subscribe(() => console.log(store.getState()) )
+
+store.dispatch({ type: 'INC' })
+store.dispatch({ type: 'INC' })
