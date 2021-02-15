22d1f715a4394bc03f02b1cb75232e7fc6cf567e
Surta <pasha.surta@mail.ru>
2021-02-15 11:29:44 +0300

    Добавил кнопку rnd, которая генерирует случайное число и передает в store тип и случайное число, которое потом можно достать из объекта action. ВАЖНО!!! reducer обязательно должен быть чистой функцией

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const reducer = (state = 0, action) => {
     switch (action.type){
         case 'INC':
             return state + 1
+        case 'RND':
+            return state + action.payload
         case 'DEC':
             return state - 1
         default:
@@ -21,8 +23,15 @@ document.getElementById('dec').addEventListener('click', () => {
     store.dispatch({ type: 'DEC' })
 })
 
+document.getElementById('rnd').addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10)
+    store.dispatch({ type: 'RND', payload })
+})
+
 const update = () => {  
     document.getElementById('counter').innerHTML = store.getState()
 }
+
+
 
 store.subscribe(update)
