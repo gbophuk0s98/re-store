af202d3162c294bf3abfa9c03496316546b04cc3
Surta <pasha.surta@mail.ru>
2021-02-15 12:55:49 +0300

    Добавил компонент counter и связал redux с react вручную, Реакт компонент Counter не знает, откуда берутся данные, он просто отрисовывает их при изменении счетчика

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import { createStore, bindActionCreators } from 'redux'
 import reducer from './reducer'
 import * as actions from './actions'
 
+import Counter from './counter'
+
 const store = createStore(reducer)
 const { dispatch, getState } = store
 
-const { inc, dec, rnd} = bindActionCreators(actions, dispatch)
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch)
 
-document.getElementById('inc').addEventListener('click', inc)
+// document.getElementById('inc').addEventListener('click', inc)
 
-document.getElementById('dec').addEventListener('click', dec)
+// document.getElementById('dec').addEventListener('click', dec)
 
-document.getElementById('rnd').addEventListener('click', () => {
-    const payload = Math.floor(Math.random() * 10)
-    rnd(payload)
-})
+// document.getElementById('rnd').addEventListener('click', () => {
+//     const payload = Math.floor(Math.random() * 10)
+//     rnd(payload)
+// })
 
 const update = () => {  
-    document.getElementById('counter').innerHTML = getState()
+    ReactDOM.render(
+        <Counter 
+            counter={getState()}
+            inc={inc}
+            dec={dec}
+            rnd={() => {
+                const payload = Math.floor(Math.random() * 10)
+                rnd(payload)
+            }}
+        />,
+         document.getElementById('root'))
 }
 
-
-
+update()
 store.subscribe(update)
