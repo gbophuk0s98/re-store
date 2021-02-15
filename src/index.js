4b468b417d6ffc65a5c552d5391fe5417f767b49
Surta <pasha.surta@mail.ru>
2021-02-15 14:33:06 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { createStore, bindActionCreators } from 'redux'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import reducer from './reducer'
-import * as actions from './actions'
 
-import Counter from './counter'
+import App from './components/app'
 
 const store = createStore(reducer)
-const { dispatch, getState } = store
 
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch)
 
-// document.getElementById('inc').addEventListener('click', inc)
-
-// document.getElementById('dec').addEventListener('click', dec)
-
-// document.getElementById('rnd').addEventListener('click', () => {
-//     const payload = Math.floor(Math.random() * 10)
-//     rnd(payload)
-// })
-
-const update = () => {  
-    ReactDOM.render(
-        <Counter 
-            counter={getState()}
-            inc={inc}
-            dec={dec}
-            rnd={() => {
-                const payload = Math.floor(Math.random() * 10)
-                rnd(payload)
-            }}
-        />,
-         document.getElementById('root'))
-}
-
-update()
-store.subscribe(update)
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+        document.getElementById('root'))
