3296d1eae02652e8a584a9a4be4f0e460f72ff95
Surta <pasha.surta@mail.ru>
2021-02-15 15:54:04 +0300

    инциализация react-redux проекта

---
+++ src/components/app/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './app.css'
+
+const App = () => {
+    return <div className="container">App</div>
+}
+
+export default App
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './spinner.css'
+
+const Spinner = () => {
+    return(
+        <div className="loadingio-spinner-spinner-fr8ux0tn0wq">
+            <div className="ldio-76zl63iqt3">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
