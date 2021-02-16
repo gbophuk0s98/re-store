83303bd97f5b0c64970e986833e736107fa826e5
Surta <pasha.surta@mail.ru>
2021-02-16 15:50:13 +0300

    Вынес отображение Booklist в отдельный функциональный компонент, создал контейнер для этого компонента, который занимается получением данных, показом спиннера или ошибки, сам буклист только отрисовывает данные

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,7 +10,23 @@ import Spinner from '../spinner'
 import compose from '../../utils'
 import './book-list.css'
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+    return(
+        <ul className="book-list">
+            {
+                books.map(book => {
+                    return(
+                        <li key={book.id}>
+                            <BookListItem book={book} />
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+class BookListContainer extends Component {
 
     componentDidMount(){
         this.props.fetchBooks()
@@ -21,22 +37,8 @@ class BookList extends Component {
         
         if (loading) return <Spinner />
         if (error) return <ErrorIndicator />
-
-        return(
-            <ul className="book-list">
-                {
-                    books.map(book => {
-                        return(
-                            <li key={book.id}>
-                                <BookListItem book={book} />
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        )
+        return <BookList books={books}/>
     }
-
 }
 
 const mapStateToProps = ({ books, loading, error }) => {
@@ -44,48 +46,12 @@ const mapStateToProps = ({ books, loading, error }) => {
 }
 
 const mapDispatchToProps = (dispatch, { bookstoreService }) => {
-    /**
-     * bindActionCreators возвращает точно объект со структурой, описанной ниже
-     * Поэтому можно вместо этого громоздкого кода:
-     *  return {
-            booksLoaded: (newBooks) => {
-                dispatch(booksLoaded(newBooks))
-            }
-        } 
-        написать:
-     * bindActionCreators({
-     *  booksLoaded
-     * } - передаем объект с action creators, dispatch)
-     */
-
-     /**
-      * А можно пойти ещё дальше и написать mapDispatchToProps = { booksLoaded }
-      * Т.к. если вместо функции в connect мы передаем объект, то этот объект попадет в качестве аргумента в bindActionCreators
-      */
-
-
-    //Вместо этого:
-
-    // return bindActionCreators({
-    //     booksLoaded,
-    //     booksRequested,
-    //     booksError,
-    // }, dispatch)
-
-    //Напишу это:
     return {
         fetchBooks: fetchBooks(bookstoreService, dispatch)
     }
 }
 
-/**
- * Экспорт, описанный ниже, альтернатива другой записи:
- * export default withBookService()(
- *  connect(mapStateToProps, mapDispatchToProps)(BookList)
- * )
- */
-
 export default compose(
     withBookstoreService(),
     connect(mapStateToProps, mapDispatchToProps)
-)(BookList)
+)(BookListContainer)
